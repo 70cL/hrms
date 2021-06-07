d7c152112debba72275802de572ba123397dc5d6
ovunc.girgin <ufukakaypia@gitlab.com>
2021-06-07 11:14:13 +0300

    fix

--- src/main/java/io/kodloma/hrms/controller/JobAdvertisementController.java
+++ src/main/java/io/kodloma/hrms/controller/JobAdvertisementController.java
@@ -7,7 +7,6 @@ import io.kodloma.hrms.entities.concrete.JobAdvertisements;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -27,7 +26,7 @@ public class JobAdvertisementController {
     }
 
     @PostMapping("/add")
-    public Result add(@Valid @RequestBody JobAdvertisements jobAdvertisements){
+    public Result add(@RequestBody JobAdvertisements jobAdvertisements){
         return jobAdvertisementService.add(jobAdvertisements);
     }
 
